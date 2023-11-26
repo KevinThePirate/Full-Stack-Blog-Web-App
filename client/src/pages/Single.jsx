663922254f59f320279c259f4a4d2b04f4a18a6d
Kevin <smithkevin1100@gmail.com>
2023-11-26 18:32:39 +0000

    Client Side Application

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from 'react-router-dom';
+import Menu from '../components/Menu';
 
 export default function Single() {
   return (
-    <div>Single</div>
+    <div className="single">
+      <div className="content">
+        <img src alt="" />
+        <div className="user">
+          <img
+            src=""
+            alt=""
+          />
+          <div className="info">
+            <span>post.username</span>
+            <p>Posted Last Week</p>
+          </div>
+
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img alt="" src={Delete} />
+          </div>
+        </div>
+        <h1>post.title</h1>
+      </div>
+      <Menu />
+    </div>
   )
 }

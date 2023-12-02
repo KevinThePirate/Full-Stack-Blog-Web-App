79984f88fca68c0cc025c099357a781ae4b53ce0
Kevin <smithkevin1100@gmail.com>
2023-12-02 18:20:40 +0000

    Added homepage get methods

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,29 @@
+import { db } from "../../api/db.js";
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat = ?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data);
+  });
+};
+
+export const getPost = (req, res) => {
+  const q =
+    "SELECT `username`,`title`,`desc`,`img`,`cat`, `date` FROM users u JOIN posts p ON u.id = p.user_id WHERE p.id = ?";
+};
+
 export const addPost = (req, res) => {
-    res.json("from controller")
-}
+  res.json("from controller");
+};
+
+export const deletePost = (req, res) => {
+  res.json("from controller");
+};
+
+export const updatePost = (req, res) => {
+  res.json("from controller");
+};
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -3,30 +3,60 @@ import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import { Link } from 'react-router-dom';
 import Menu from '../components/Menu';
+import { useLocation } from 'react-router-dom';
+import axios from "axios";
+import moment from "moment";
+import { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from "../context/authContext"
 
 export default function Single() {
+
+  const [post, setPost] = useState([]); // posts is an array of objects
+
+  const location = useLocation()
+
+  const postID = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postID}`);
+        setPost(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postID]);
+
   return (
     <div className="single">
       <div className="content">
         <img src alt="" />
         <div className="user">
           <img
-            src=""
+            src={post?.img}
             alt=""
           />
           <div className="info">
-            <span>post.username</span>
-            <p>Posted Last Week</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
 
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img alt="" src={Delete} />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>post.title</h1>
+        <h1>{post.title}</h1>
       </div>
       <Menu />
     </div>

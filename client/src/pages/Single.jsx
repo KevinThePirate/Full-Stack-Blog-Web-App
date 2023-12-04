47259cf706ff5b085ef300f25ee6df70930a61e2
Kevin <smithkevin1100@gmail.com>
2023-12-04 11:48:10 +0000

    added delete method and recc'd posts

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Menu from '../components/Menu';
 import { useLocation } from 'react-router-dom';
 import axios from "axios";
@@ -12,6 +12,8 @@ import { AuthContext } from "../context/authContext"
 
 export default function Single() {
 
+  const navigate = useNavigate();
+
   const [post, setPost] = useState([]); // posts is an array of objects
 
   const location = useLocation()
@@ -19,6 +21,17 @@ export default function Single() {
   const postID = location.pathname.split("/")[2];
 
   const { currentUser } = useContext(AuthContext);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postID}`);
+      navigate("/");
+    }
+    catch (err) {
+      console.log(err);
+    }
+
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,10 +49,10 @@ export default function Single() {
   return (
     <div className="single">
       <div className="content">
-        <img src alt="" />
+        <img src={post?.img} alt="" />
         <div className="user">
           <img
-            src={post?.img}
+            src={post?.userImg || "https://i.imgur.com/6VBx3io.png"}
             alt=""
           />
           <div className="info">
@@ -52,13 +65,15 @@ export default function Single() {
               <Link to={`/write?edit=2`} state={post}>
                 <img src={Edit} alt="" />
               </Link>
-              <img src={Delete} alt="" />
+              <img src={Delete} onClick={handleDelete} alt="" />
             </div>
           )}
         </div>
         <h1>{post.title}</h1>
+        <div className="desc">
+          <p>{post.desc}</p> </div>
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   )
 }
